a62e1c73e3e5e81bda920347ee1ff8163a7de704
uriahome <ryouma_1106@yahoo.co.jp>
2020-09-04 00:01:35 +0900

    ポーションを生成する釜の建築物カードを追加

---
+++ Assets/Script/Building.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    //建物系のカードにつける
+    //体力とかはMonsterの方もつけるのでそっちにまかせる
+    public GameObject ShotObject;//発射するオブジェクト
+    public float span;
+    public float delta;
+
+    public GameObject Player;
+    public PlayerController PCon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delta = 0;
+        Player = GameObject.Find("PlayerMaster");
+        PCon = Player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(span < delta){
+            delta = 0;
+            MakeObj();//オブジェクトを作る
+        }else{
+             this.delta += Time.deltaTime;//経過時間の加算(いつものやつ)
+        }
+    }
+
+    void MakeObj(){//ポーションとかを出す
+        GameObject Fire = Instantiate(ShotObject) as GameObject;//炎の生成
+        Fire.gameObject.tag = "PlayerAttack";
+        Fire.transform.position = new Vector3(this.transform.position.x,this.transform.position.y, this.transform.position.z);//自分の場所に出す
+        ShockWave SObj = Fire.GetComponent<ShockWave>();
+        SObj.Power = PCon.Attack;//攻撃力の代入
+        SObj.Move();
+    }
+}
