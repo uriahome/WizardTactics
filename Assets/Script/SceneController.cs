d06826b481222157d4434bee3ce709b5e79454fa
uriahome <ryouma_1106@yahoo.co.jp>
2020-10-08 16:28:36 +0900

    仮のタイトル画面を作成してメインの戦闘画面を読み込む処理の追加

---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ChangeScene(){
+       // SceneManager.LoadScene("BattleSceme",LoadSceneMode,Single);//メインの戦闘シーンを読み込む
+       Debug.Log("SceneLoad");//シーンの読み込み(非同期)
+       AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("BattleScene");
+    }
+
+}
