25cf23de437b6c1c36ffb772fa2b11ef24534e72
uriahome <ryouma_1106@yahoo.co.jp>
2020-12-14 23:00:21 +0900

    全カードリストを確認できる機能をタイトル画面に追加した。 今後はここもわかり易い誘導を仕掛けたい

---
+++ Assets/Script/ReviewButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReviewButton : MonoBehaviour
+{
+    public GameObject ButtonListCanvas;
+    public GameObject DeckCancas;
+    public GameObject TitleCanvas;
+    public bool ViewFlag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ViewFlag = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ViewFlag){
+            ButtonListCanvas.SetActive(true);
+            DeckCancas.SetActive(false);
+            TitleCanvas.SetActive(false);
+        }else{
+            ButtonListCanvas.SetActive(false);
+            DeckCancas.SetActive(true);
+            TitleCanvas.SetActive(true);
+        }
+    }
+
+    public void OnClick(){
+        ViewFlag = !ViewFlag;
+    }
+}
