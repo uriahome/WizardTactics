ee324586bdf187af49820e710580469127842207
uriahome <ryouma_1106@yahoo.co.jp>
2021-01-03 23:30:12 +0900

    シーンの読み込みに暗転処理を実装した

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
@@ -11,9 +12,31 @@ public class SceneController : MonoBehaviour
     //public AudioClip BGM_title;//戦闘シーンのBGMの管理もここで行う
     public AudioClip sound;
     public AudioSource audio;
+
+    public GameObject FadeObject;
+    public Image FadeImage;//フェードアウト用のイメージ
+    public bool IsFadeOut;
+    public float red,green,blue,alpha;//画像の赤、緑、青、透明度
+    public float FadeSpeed;
+    public string CurrentSceneName;//現在のシーンの名前
     void Start()
     {
         audio = GetComponent<AudioSource>();
+
+        CurrentSceneName = SceneManager.GetActiveScene().name;
+        if(CurrentSceneName =="Title"){
+            FadeObject = GameObject.Find("DeckViewCanvas/FadeImage");
+        }else{
+            FadeObject = GameObject.Find("DeckCanvas/FadeImage");
+        }
+        FadeImage = FadeObject.GetComponent<Image>();
+        red = FadeImage.color.r;
+        green = FadeImage.color.g;
+        blue = FadeImage.color.b;
+        FadeImage.color = new Color(red,green,blue,0);
+        alpha = FadeImage.color.a;
+        FadeImage.enabled = false;
+
         /*audio1 = GetComponent<AudioSource>();
         audio1.volume = 0.5f;
         audio1.Stop();//今流れているのを止めてから流す
@@ -23,10 +46,27 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(IsFadeOut){
+            StartFadeOut();
+        }
     }
+
+    void StartFadeOut(){
+        FadeImage.enabled = true;
+        alpha += FadeSpeed;
+        FadeImage.color = new Color(red,green,blue,alpha);
+        if(alpha >= 1){
+            IsFadeOut = false;
+        }
+    }
+
+    /*public void ChangeFade(){
+        IsFadeOut = true;
+    }*/
 
     public void ChangeScene()
     {
+        IsFadeOut = true;
         audio.PlayOneShot(sound);//効果音を再生する
         // SceneManager.LoadScene("BattleSceme",LoadSceneMode,Single);//メインの戦闘シーンを読み込む
         Debug.Log("SceneLoad");//シーンの読み込み(非同期)
@@ -39,6 +79,7 @@ public class SceneController : MonoBehaviour
 
     public void ChangeTitle()
     {//タイトルに戻ってくるとき
+        IsFadeOut = true;
         audio.PlayOneShot(sound);//効果音を再生する
         Debug.Log("SceneLoad");//シーンの読み込み(非同期)
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Title");
