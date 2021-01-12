d0b01fdeb65825747a1cff782726cb2cc41b94a3
uriahome <ryouma_1106@yahoo.co.jp>
2021-01-12 23:37:03 +0900

    csvの扱いテスト

--- Assets/Script/Read_csv.cs
+++ Assets/Script/Read_csv.cs
@@ -6,6 +6,16 @@ using System.IO;
 public class Read_csv : MonoBehaviour
 {
     // Start is called before the first frame update
+    //public Monster Knight;
+    public string Name;
+    public float Hp;
+    public int Attack;
+    public float Speed;
+    public float MaxSpeed;
+    public float AttackSpan;
+
+    public int MonsterNumber;
+
     public TextAsset csvFile;//元データ
     List<string[]> csvData = new List<string[]>();//読み込んだ全てのデータ
     public List<string> Parameters =  new List<string>();//読み込んだ1キャラのデータ
@@ -21,12 +31,13 @@ public class Read_csv : MonoBehaviour
         }
 
 
-        Debug.Log(csvFile.text);
-        Debug.Log(csvData.Count);
-        Debug.Log(csvData[0][0]);//Name
-        Debug.Log(csvData[0][1]);//Hp
+        //Debug.Log(csvFile.text);
+        //Debug.Log(csvData.Count);
+        //Debug.Log(csvData[0][0]);//Name
+        //Debug.Log(csvData[0][1]);//Hp
         //Debug.Log(csvData[1]);
-        Parameters = MonsterStatus(1);
+        Parameters = MonsterStatus(MonsterNumber);//1番のデータのキャラステータスを読み込む
+        SetStatus();//ステータスをセットする
         //MonsterStatus(2);
     }
 
@@ -44,5 +55,14 @@ public class Read_csv : MonoBehaviour
         }
 
         return MonsterData;
+    }
+
+    public void SetStatus(){//ステータスをセットする
+        Name = Parameters[0];
+        Hp =  float.Parse(Parameters[1]);
+        Attack = int.Parse(Parameters[2]);
+        Speed = float.Parse(Parameters[3]);
+        MaxSpeed = float.Parse(Parameters[4]);
+        AttackSpan = float.Parse(Parameters[5]);
     }
 }
