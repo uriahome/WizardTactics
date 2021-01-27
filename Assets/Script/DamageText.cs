fa39e981d15be3248fadf17a9d1ae237e18e839e
uriahome <ryouma_1106@yahoo.co.jp>
2021-01-27 22:09:10 +0900

    ダメージテキストのサイズがビルド後だと小さかったのでスケールを2倍にした マップボタンのテキストを見やすくした

--- Assets/Script/DamageText.cs
+++ Assets/Script/DamageText.cs
@@ -8,6 +8,7 @@ public class DamageText : MonoBehaviour
     public Rigidbody2D rigid2d;
     void Start()
     {
+        //上に跳ねるイメージ
         rigid2d = GetComponent<Rigidbody2D>();
         rigid2d.AddForce(new Vector3(0,300,0));
         StartCoroutine("DestroyObject");
@@ -19,9 +20,8 @@ public class DamageText : MonoBehaviour
         
     }
 
-    IEnumerator DestroyObject()
+    IEnumerator DestroyObject()//0.5秒後に消える
     {
-        
         yield return new WaitForSeconds(0.5f);
         Destroy(this.gameObject);
     }
